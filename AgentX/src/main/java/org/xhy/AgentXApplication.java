d8718ebbfd8124204af730dbd9dfceab07a86759
Xhy <xhyovo@qq.com>
2025-03-19 10:12:48 +0800

    init

---
+++ AgentX/src/main/java/org/xhy/AgentXApplication.java
@@ -0,0 +1,15 @@
+package org.xhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AgentX应用启动类
+ */
+@SpringBootApplication
+public class AgentXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentXApplication.class, args);
+    }
+}
